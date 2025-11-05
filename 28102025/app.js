39a19672caa7fe846af4a229aae4f8a0a74c53dd
juan-gg1 <juangutrey@gmail.com>
2025-11-05 08:42:59 -0600

    Cambios de servidor plano en http y node

--- 28102025/app.js
+++ 28102025/app.js
@@ -1,1 +1,14 @@
-consts
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Holaaaa, este es un Servidor Express ');
+});
+
+const port = 3025;
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
